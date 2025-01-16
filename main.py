Fixing missing gradient

main.py
@@ -79,20 +79,21 @@ for epoch in range(epochs):
 
         buffer.add(
             (
-                obs.detach(),
-                obs_pred.detach(),
-                z.detach(),
-                z_pred.detach(),
-                reward.detach(),
-                reward_pred.detach(),
-                done.detach(),
-                cont_pred.detach(),
+                obs,
+                obs_pred,
+                z,
+                z_pred,
+                reward,
+                reward_pred,
+                done,
+                cont_pred,
+                h,
             )
         )
 
-        obs = next_obs.detach()
-        h = h_next.detach()
-        action = action_next.detach()
+        obs = next_obs
+        h = h_next
+        action = action_next
 
     # Train the model
     print("==============================")
@@ -107,6 +108,7 @@ for epoch in range(epochs):
         reward_pred_batch,
         done_batch,
         cont_pred_batch,
+        h_batch,
     ) = zip(*batch)
 
     obs_batch = torch.stack(obs_batch)
@@ -117,25 +119,23 @@ for epoch in range(epochs):
     reward_pred_batch = torch.stack(reward_pred_batch)
     done_batch = torch.stack(done_batch)
     cont_pred_batch = torch.stack(cont_pred_batch)
+    h_batch = torch.stack(h_batch)
 
     batch = (
-        obs_batch.requires_grad_(True),
-        obs_pred_batch.requires_grad_(True),
-        z_batch.requires_grad_(True),
-        z_pred_batch.requires_grad_(True),
-        reward_batch.requires_grad_(True),
-        reward_pred_batch.requires_grad_(True),
-        done_batch.requires_grad_(True),
-        cont_pred_batch.requires_grad_(True),
+        obs_batch,
+        obs_pred_batch,
+        z_batch,
+        z_pred_batch,
+        reward_batch,
+        reward_pred_batch,
+        done_batch,
+        cont_pred_batch,
+        h_batch,
     )
 
     world_model_loss = dreamer.train_world_model(batch)
-    actor_loss, critic_loss, td_errors = dreamer.train_actor_critic(batch[0])
+    actor_loss, critic_loss, td_errors = dreamer.train_actor_critic(batch[0], batch[-1])
     buffer.update_priorities(indices, td_errors.cpu().detach().numpy())
-
-    dreamer.model_scheduler.step(world_model_loss)
-    dreamer.actor_scheduler.step(actor_loss)
-    dreamer.critic_scheduler.step(critic_loss)
 
     print("World Model Loss", world_model_loss)
     print("Actor Loss", actor_loss)

ptnn3/DreamerV3.py
@@ -207,6 +207,7 @@ class DreamerV3(nn.Module):
             reward_pred,
             done,
             cont_pred,
+            _,
         ) = batch
 
         # Compute the prediction losses
@@ -234,6 +235,7 @@ class DreamerV3(nn.Module):
         self.model_optimizer.zero_grad()
         total_loss.backward()
         self.model_optimizer.step()
+        self.model_scheduler.step(total_loss)
 
         return total_loss.item()
 
@@ -248,13 +250,12 @@ class DreamerV3(nn.Module):
 
         return kl_loss
 
-    def train_actor_critic(self, obs):
+    def train_actor_critic(self, obs, h):
         eta = 3e-4  # Entropy weight
         gamma = 0.997  # Discount factor
         lambda_ = 0.95  # Lambda parameter
 
         z = self.encoder(obs)
-        h = torch.randn(obs.size(0), self.h_dim).to(obs.device)
 
         action_t = []
         value_t = []
@@ -310,18 +311,19 @@ class DreamerV3(nn.Module):
         entropy_regularization = -eta * torch.sum(entropy, dim=0).mean()
 
         actor_loss = policy_gradient_loss + entropy_regularization
+        critic_loss = F.mse_loss(value_t, lambda_returns)
+        total_loss = actor_loss + critic_loss
+
         self.actor_optimizer.zero_grad()
-        actor_loss.backward()
+        total_loss.backward()
         self.actor_optimizer.step()
 
-        value_loss = F.mse_loss(value_t, lambda_returns)
-        self.critic_optimizer.zero_grad()
-        value_loss.backward()
-        self.critic_optimizer.step()
+        self.actor_scheduler.step(actor_loss)
+        self.critic_scheduler.step(critic_loss)
 
         return (
             actor_loss.item(),
-            value_loss.item(),
+            critic_loss.item(),
             torch.abs((value_t - lambda_returns).mean(dim=0)),
         )
 